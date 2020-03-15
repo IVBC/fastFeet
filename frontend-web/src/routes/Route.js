6c05cc327dedf4504bf0a237b15c05a61f8909ca
isaquevilson <isaque.vilson27@gmail.com>
2020-03-15 01:10:28 -0400

    adding app base web

---
+++ frontend-web/src/routes/Route.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Route, Redirect } from 'react-router-dom';
+
+export default function RouteWrapper({
+  component: Component,
+  isPrivate,
+  ...rest
+}) {
+  const { signed } = true;
+
+  if (!signed && isPrivate) {
+    return <Redirect to="/" />;
+  }
+
+  if (signed && !isPrivate) {
+    return <Redirect to="/dashboard" />;
+  }
+
+  // const Layout = signed ? DefaultLayout : AuthLayout;
+
+  return <Route {...rest} component={Component} />;
+}
+RouteWrapper.propTypes = {
+  isPrivate: PropTypes.bool,
+  component: PropTypes.oneOfType([PropTypes.element, PropTypes.func])
+    .isRequired,
+};
+
+RouteWrapper.defaultProps = {
+  isPrivate: false,
+};
