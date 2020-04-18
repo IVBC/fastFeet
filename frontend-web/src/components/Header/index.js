93e5519abbc5f3bd51d273c500e965ff3b89ff0d
isaquevilson <isaque.vilson27@gmail.com>
2020-04-18 07:14:38 -0400

    responsive

--- frontend-web/src/components/Header/index.js
+++ frontend-web/src/components/Header/index.js
@@ -1,38 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 
 import logo from '~/assets/logo.svg';
 
-import { Container, Content, NavBar, Profile } from './styles';
+import {
+  Container,
+  Content,
+  NavBar,
+  Profile,
+  MenuToggle,
+  MenuOne,
+  MenuTwo,
+  MenuThree,
+} from './styles';
 import { signOut } from '~/store/modules/auth/actions';
 
 export default function Header() {
   const dispatch = useDispatch();
+  const [showMenu, setShowMenu] = useState(false);
   const profile = useSelector(state => state.user.profile);
 
   function handleSignOut() {
     dispatch(signOut());
   }
 
+  const handleShowMenu = () => {
+    setShowMenu(!showMenu);
+  };
+
   return (
-    <Container>
+    <Container on={showMenu ? 1 : 0}>
       <Content>
         <nav>
           <img src={logo} alt="FastFeet" />
-          <NavBar>
+          <NavBar
+            onClick={() => {
+              setShowMenu(false);
+            }}
+          >
             <NavLink to="/deliveries">ENCOMENDAS</NavLink>
             <NavLink to="/deliverers">ENTREGADORES</NavLink>
             <NavLink to="/recipients">DESTINATÁRIOS</NavLink>
             <NavLink to="/problems">PROBLEMAS</NavLink>
           </NavBar>
         </nav>
+
         <Profile>
           <strong>{profile.name}</strong>
           <button type="button" onClick={handleSignOut}>
             Sair do sistema
           </button>
         </Profile>
+
+        <MenuToggle onClick={handleShowMenu}>
+          <MenuOne />
+          <MenuTwo />
+          <MenuThree />
+        </MenuToggle>
       </Content>
     </Container>
   );
--- frontend-web/src/components/Header/styles.js
+++ frontend-web/src/components/Header/styles.js
@@ -1,14 +1,29 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import { darken } from 'polished';
 
-export const Container = styled.div`
-  background: #fff;
-  border: 1px solid #dddddd;
-  padding: 0 30px;
+export const NavBar = styled.div`
+  border-left: 1px solid #ddd;
+  padding-left: 30px;
+  height: 32px;
+  display: flex;
+  align-items: center;
+  a {
+    margin-right: 20px;
+    font-size: 1.6rem;
+    font-weight: bold;
+    color: #999;
+    &.active {
+      color: #444;
+    }
+  }
+  @media (max-width: 890px) {
+    display: none;
+  }
 `;
 
 export const Content = styled.div`
   height: 64px;
+  /* background-color: green; */
   display: flex;
   align-items: center;
   justify-content: space-between;
@@ -19,23 +34,6 @@ export const Content = styled.div`
       width: 135px;
       height: 26px;
       padding-right: 30px;
-    }
-  }
-`;
-
-export const NavBar = styled.div`
-  border-left: 1px solid #ddd;
-  padding-left: 30px;
-  height: 32px;
-  display: flex;
-  align-items: center;
-  a {
-    margin-right: 20px;
-    font-size: 15px;
-    font-weight: bold;
-    color: #999;
-    &.active {
-      color: #444;
     }
   }
 `;
@@ -59,4 +57,104 @@ export const Profile = styled.div`
       color: ${darken(0.15, '#de3b3b')};
     }
   }
+
+  @media (max-width: 890px) {
+    display: none;
+  }
+`;
+
+export const MenuToggle = styled.div`
+  width: 40px;
+  height: 30px;
+
+  @media (min-width: 890px) {
+    display: none;
+  }
+`;
+export const MenuOne = styled.div`
+  background-color: #666666;
+  height: 5px;
+  width: 100%;
+  margin: 6px auto;
+  transition-duration: 0.3s;
+`;
+export const MenuTwo = styled.div`
+  background-color: #666666;
+  height: 5px;
+  width: 100%;
+  margin: 6px auto;
+  transition-duration: 0.3s;
+`;
+export const MenuThree = styled.div`
+  background-color: #666666;
+  height: 5px;
+  width: 100%;
+  margin: 6px auto;
+  transition-duration: 0.3s;
+`;
+
+export const Container = styled.div`
+  background: #fff;
+  border: 1px solid #dddddd;
+  padding: 0 30px;
+  ${({ on }) =>
+    on &&
+    css`
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100vw;
+      height: 100vh;
+      background-color: #f5f5f5;
+      z-index: 10;
+      & ${NavBar} {
+        display: flex;
+        flex-direction: column;
+        border-left: 0 solid #ddd;
+
+        /* background-color: red; */
+
+        a {
+          transition-duration: 0.5s;
+          font-size: 3rem;
+          line-height: 6rem;
+          display: block;
+        }
+      }
+
+      & ${Profile} {
+        display: flex;
+        button {
+          text-align: center;
+        }
+      }
+
+      & ${Content} {
+        justify-content: space-around;
+        height: 100%;
+        flex-direction: column;
+        nav {
+          flex-direction: column;
+          img {
+            padding: 0;
+            margin-bottom: 40px;
+          }
+        }
+      }
+      & ${MenuToggle} {
+        position: absolute;
+        top: 17px;
+        right: 29px;
+      }
+
+      & ${MenuOne} {
+        transform: rotate(45deg) translate(7px, 7px);
+      }
+      & ${MenuTwo} {
+        opacity: 0;
+      }
+      & ${MenuThree} {
+        transform: rotate(-45deg) translate(8px, -9px);
+      }
+    `}
 `;
